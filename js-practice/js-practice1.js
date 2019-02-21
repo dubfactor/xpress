17df5474088feaa1b411b92b2164073ddad4e8f3
Richard Arnett <richard@arnett.org>
2019-02-20 23:56:47 -0600

    Wed 20th classwork excercises

---
+++ js-practice/js-practice1.js
@@ -0,0 +1,26 @@
+// var x=1;
+// while (x <=100) {
+// console.log("x is " + x);
+// x = x+2;
+// }
+
+// for (intialExpression; condition; incrementExpression)  {
+
+
+// }
+
+// for (var x = 1; x <= 5; x++)    {
+//     console.log(x)
+// }
+// var x = "banana";
+// for (x = 0; x.length(banana); x++); {
+//     console.log(x);
+// }
+
+var i = "banana";
+for (var i = 0; x <= i.length; i++;)   {
+    console.log("i is " + i);
+}
+
+
+
