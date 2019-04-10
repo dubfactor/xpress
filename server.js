5cb9a15bc4ee42eb06425da40ed9f361a6ac201d
Richard Arnett <richard@arnett.org>
2019-04-10 17:48:24 -0500

    updated without /node_module

--- server.js
+++ server.js
@@ -1,76 +1,83 @@
-var express = require('express');
-const app = express();
-const morgan = require('morgan')
-const bodyParser = require('body-parser');
+var express = require('express')
+var app = express()
 
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( {extended: true} ) );
+var morgan = require('morgan')
+app.use(morgan('dev'))
 
-const port = process.env.PORT || 3001;
+var bodyParser = require('body-parser')
 
-let toDoArray = [
-    { id: 1, description: "Call mom", isComplete: false },
-    { id: 2, description: "Buy groceries", isComplete: false },
-    { id: 3, description: "Go to movies", isComplete: false },
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+var port = process.env.PORT || 5500
+
+var toDoArray = [
+    {id: 1, description: "Call mom", isComplete: false},
+    {id: 2, description: "Buy groceries", isComplete: false},
+    {id: 3, description: "Go to movies", isComplete: false}
 ]
 
-app.get('/', (req, res) => {
-    res.send(`SUPER ❄️UNIQUE❄️ TODO APP`);
-});
+app.get('/', function(req, res){
+    res.send('You have reached my ToDos API!!')
+})
 
-app.get('/todos', (req, res) => {
+app.get('/todos', function(req, res){
     res.send(toDoArray);
-});
+})
 
-app.post('/todos', (req, res) => {
-
-    let newToDo = {
+app.post('/todos', function(req, res){
+    // create new todo object based on the
+    // data received by this API 
+    let newTodo = {
         id: parseInt(req.body.id),
-        description: req.body.description,
+        description:  req.body.description,
         isComplete: false
     }
+    // add the new todo to the todoArray
+    toDoArray.push(newTodo)
+    // let's see how our toDoArray looks now
+    console.log(toDoArray)
+    // set the status code which is sent back 
+    // and send the new todo to the client
+    res.status(201).send(newTodo)
+})
 
-    toDoArray.push(newToDo);
-    console.log(newtoDo);
-    console.log(toDoArray);
-    // Finish the http request
-    res.status(201).send(newToDo);
-});
+app.put('/todos/:todoid', function(req, res){
+    // the req.params object contains all the params (e.g. :todoid)
+    console.log('the req.params object is :', req.params)
 
-app.delete('/todos/:todoid', (req, res) => {
-    let requestedTodoId = parseInt(req.params.todoid);
-    // let's find the requested todo
-    var requestedTodoIndex = toDoArray.findIndex( (elem) => {
-        return elem.id === requestedTodoId;
-    });
-    // console.log('the requestedTodoIndex is', requestedTodoIndex);
-    // console.log(toDoArray);
-    if (requestedTodoIndex == undefined || requestedTodoIndex == -1) {
-        res.status(400).send('The requested todo id was not found');
-    } else {
-        toDoArray.splice(requestedTodoIndex, newFunction(2));
-    }
-    res.send(toDoArray);
-    // console.log(windows.json.toDoArray);
-});
-// console.log(windows.json.requestedTodoId)
-app.put('/todos/:todoid', (req, res) => {
-    let requestedTodoId = parseInt(req.params.todoid);
-    // let's find the requested todo
-    var requestedTodo = toDoArray.find( (elem) => {
-        return elem.id === requestedTodoId;
-    });
-    requestedTodo.isComplete = !requestedTodo.isComplete;
-    res.send(requestedTodo);
-});
+    // we parseInt this param, because it looks like a string
+    let requestedToDoId = parseInt(req.params.todoid)
 
-app.listen(port, () => {
-    console.log(`Started ToDo API on port ⚡️  ${port}`);
-});
+    // let's find the todo in the array that matches the todoId passed in
+    var requestedToDo = toDoArray.find(function(todo){
+        return todo.id === requestedToDoId
+    })
 
+    // toggle the completion status
+    requestedToDo.isComplete = !requestedToDo.isComplete;
+    
+    // return the toggled todo as confirmation
+    res.status(200).send(requestedToDo)
+})
 
-function newFunction() {
-    return 1;
-}
+app.delete('/todos/:todoid', function(req, res){
+    var requestedToDoId = parseInt(req.params.todoid)
 
+    // Find the index of the requested todo in the toDoArray
+    // The findIndex function loops over the toDoArray
+    var requestedToDoIndex = toDoArray.findIndex(function(todo){
+        return todo.id === requestedToDoId;
+    })
+
+    // remove the requested todo from the toDoArray
+    toDoArray.splice(requestedToDoIndex, 1)
+
+    // send the toDoArray as a confirmation
+    res.send(toDoArray)
+})
+
+app.listen(port, function(){
+    console.log(`Started ToDo API on port ${port}`)
+})
