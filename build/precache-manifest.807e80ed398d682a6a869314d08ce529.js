05aaaab80f87290f99749a4908e241e0695fb977
Richard Arnett <richard@arnett.org>
2019-05-30 18:19:31 -0500

    Traversy-Todo-CrashCourse-Completed!

---
+++ build/precache-manifest.807e80ed398d682a6a869314d08ce529.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626b27aaf3d1d7343222ccbbe0804f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "044df97131815edbe297",
+    "url": "/static/css/main.5d61c792.chunk.css"
+  },
+  {
+    "revision": "d7c65a71e03fe9ca64ef",
+    "url": "/static/js/2.a0e78736.chunk.js"
+  },
+  {
+    "revision": "044df97131815edbe297",
+    "url": "/static/js/main.628951ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
